55f271db0c6ef77c76cfb59c763b1c1f898de9fb
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-08-10 15:03:23 -0700

    Add Spring initial files

---
+++ src/main/java/com/kangsik/TrizicRESTfulAPI/TrizicResTfulApiApplication.java
@@ -0,0 +1,12 @@
+package com.kangsik.TrizicRESTfulAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrizicResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrizicResTfulApiApplication.class, args);
+	}
+}
