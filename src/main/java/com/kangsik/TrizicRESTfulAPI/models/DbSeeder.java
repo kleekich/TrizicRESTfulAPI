c33ca9fdc8375d6fdc1d3b102322cd16bb2db1f7
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-08-10 15:55:29 -0700

    Add .gitignore file to ignore bin directory

--- src/main/java/com/kangsik/TrizicRESTfulAPI/models/DbSeeder.java
+++ src/main/java/com/kangsik/TrizicRESTfulAPI/models/DbSeeder.java
@@ -32,13 +32,29 @@ public class DbSeeder implements CommandLineRunner {
 						"2017-03-01",//CreatedOn
 						"TAXABLE",//modelTye
 						"QUARTERLY",//rebalance
-						"ADVISORID REQUIRED", Arrays.asList(
+						"e0fe83a1-7512-432f-ba66-d2ae677272c1", Arrays.asList(
 								new AssetAllocation("AAPL", 30),
 								new AssetAllocation("GOOG", 20),
 								new AssetAllocation("IBM", 15),
 								new AssetAllocation("FB", 25)
 						)
 					);
+		Portfolio p2 = new Portfolio(
+
+				"Portfolio example 2",//name
+				"example model with misc stocks",//desc
+				5,//cP
+				2,//dP
+				"2017-03-02",//CreatedOn
+				"TAXABLE",//modelTye
+				"ANNUAL",//rebalance
+				"549a368e-1671-4d84-8463-af2bc32f1dea", Arrays.asList(
+						new AssetAllocation("COCK", 15),
+						new AssetAllocation("PEP", 15),
+						new AssetAllocation("CMG", 30),
+						new AssetAllocation("MCD", 35)
+				)
+			);
 				
 	}
 }
